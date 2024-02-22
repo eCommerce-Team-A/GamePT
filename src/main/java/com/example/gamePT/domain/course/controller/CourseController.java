d2263248cf0224dc8a4ebd77e7b460d5eaa0fd67
wintersky2 <emsw747@gmail.com>
2024-02-22 18:50:49 +0900

    detail페이지 구현중

--- src/main/java/com/example/gamePT/domain/course/controller/CourseController.java
+++ src/main/java/com/example/gamePT/domain/course/controller/CourseController.java
@@ -6,8 +6,10 @@ import com.example.gamePT.domain.course.service.CourseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,4 +33,12 @@ public class CourseController {
                 courseCreateForm.getInformation(), courseCreateForm.getPrice());
         return String.format("redirect:/course/detail/%s", course.getId());
     }
+
+    @GetMapping("/detail/{id}")
+    public String showCourseDetail(@PathVariable(value = "id")Long id, Model model){
+        Course course = this.courseService.findCourseById(id);
+        model.addAttribute("course",course);
+        return "course_detail";
+    }
+
 }
