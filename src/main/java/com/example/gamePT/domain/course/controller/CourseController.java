838e123f3d99855f7c6469eb25f332bbe9e11476
wintersky2 <emsw747@gmail.com>
2024-02-26 18:52:09 +0900

    디테일 페이지 프론트까지

--- src/main/java/com/example/gamePT/domain/course/controller/CourseController.java
+++ src/main/java/com/example/gamePT/domain/course/controller/CourseController.java
@@ -3,23 +3,26 @@ package com.example.gamePT.domain.course.controller;
 import com.example.gamePT.domain.course.entity.Course;
 import com.example.gamePT.domain.course.entity.CourseCreateForm;
 import com.example.gamePT.domain.course.service.CourseService;
+import com.example.gamePT.domain.user.entity.SiteUser;
+import com.example.gamePT.domain.user.repository.UserRepository;
+import com.example.gamePT.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/course")
 public class CourseController {
     private final CourseService courseService;
+    private final UserRepository userRepository; // User 불러오기용 임시 의존성 주입. (createCourse 메서드에서 추후 작업 필요)
 
     @GetMapping("/create")
     public String createCourse() {
@@ -27,22 +30,32 @@ public class CourseController {
     }
 
     @PostMapping("/create")
-    public String createCourse(@Valid CourseCreateForm courseCreateForm, BindingResult bindingResult) {
+    public String createCourse(@Valid CourseCreateForm courseCreateForm, BindingResult bindingResult, Principal principal) {
+        //임시 siteUser 부분 시작
+        SiteUser siteUser = null;
+        if (principal != null) {
+            Optional<SiteUser> _siteUser = this.userRepository.findByUsername(principal.getName());
+            if (_siteUser.isPresent()) {
+                siteUser = _siteUser.get();
+            }
+        }
+        // 임시 siteUser 부분 끝
+
         if (bindingResult.hasErrors()) {
             return "course/course_create_form";
         }
-        Course course = this.courseService.createCourse(courseCreateForm.getExpertname(), courseCreateForm.getGameCategoryname(), courseCreateForm.getName(),
-                courseCreateForm.getInformation(), courseCreateForm.getPrice());
+        Course course = this.courseService.createCourse(siteUser, courseCreateForm.getGameCategoryname(), courseCreateForm.getName(),
+                courseCreateForm.getIntroduce(), courseCreateForm.getCurriculum(), courseCreateForm.getPrice());
         return String.format("redirect:/course/detail/%s", course.getId());
     }
 
     @GetMapping("/detail/{id}")
-    public String showCourseDetail(@PathVariable(value = "id")Long id, Model model){
+    public String showCourseDetail(@PathVariable(value = "id") Long id, Model model) {
         Course course = this.courseService.findCourseById(id);
         List<Course> courseList = this.courseService.findAllCourse();
 
         model.addAttribute("courseList", courseList);
-        model.addAttribute("course",course);
+        model.addAttribute("course", course);
 
         return "course/course_detail";
     }
--- src/main/java/com/example/gamePT/domain/course/entity/CourseCreateForm.java
+++ src/main/java/com/example/gamePT/domain/course/entity/CourseCreateForm.java
@@ -1,14 +1,15 @@
 package com.example.gamePT.domain.course.entity;
 
+import com.example.gamePT.domain.user.entity.SiteUser;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class CourseCreateForm {
-    private String expertname;  //임시로 (전문가 이름) 삽입 추후 siteUser.expert 객체 추가 필요
+public class CourseCreateForm {//임시로 (전문가 이름) 삽입 추후 siteUser.expert 객체 추가 필요
     private String gameCategoryname; // 임시로 (카테고리 이름) 삽입 추후 gameCategory 객체추가 필요
     private String name;
-    private String information;
+    private String introduce;
+    private String curriculum;
     private Integer price;
 }
