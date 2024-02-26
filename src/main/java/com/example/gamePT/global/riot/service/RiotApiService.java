2b3b2cecbc96f54134ef768e78d767fe6163120c
5binn <dhxnejqls13@naver.com>
2024-02-26 13:57:51 +0900

    듀오게시판#8

--- src/main/java/com/example/gamePT/global/riot/service/RiotApiService.java
+++ src/main/java/com/example/gamePT/global/riot/service/RiotApiService.java
@@ -1,6 +1,5 @@
 package com.example.gamePT.global.riot.service;
 
-import com.example.gamePT.global.riot.entity.ChampionDTO;
 import com.example.gamePT.global.riot.entity.SummonerDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +14,11 @@ import java.net.URLEncoder;
 @RequiredArgsConstructor
 public class RiotApiService {
 
-    private String summonerApi = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name";
+
     private String myKey = "RGAPI-fedd0ae5-214d-4ef3-8d8b-1359a2261f70";
 
     public SummonerDTO getSummoner(String name) {
-
+        String summonerApi = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name";
         try {
             String encodedName = URLEncoder.encode(name, "UTF-8"); //공백을 처리하기 위해 인코딩
             String url = String.format("%s/%s?api_key=%s", summonerApi, encodedName, myKey);
@@ -43,27 +42,4 @@ public class RiotApiService {
         return null;
     }
 
-    public ChampionDTO getChampion(int key) {
-        try {
-            String encodedName = URLEncoder.encode(key, "UTF-8"); //공백을 처리하기 위해 인코딩
-            String url = String.format("%s/%s?api_key=%s", summonerApi, encodedName, myKey);
-            URL apiUrl = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
-            connection.setRequestMethod("GET");
-
-            int responseCode = connection.getResponseCode();
-
-            //요청 성공 시
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                ObjectMapper objectMapper = new ObjectMapper();
-                SummonerDTO summonerDTO = objectMapper.readValue(connection.getInputStream(), SummonerDTO.class);
-                return summonerDTO;
-            } else {
-                return null;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 }
