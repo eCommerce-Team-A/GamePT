e08b680e2be1c5381b50d49fdcd764c8f085b33a
5binn <dhxnejqls13@naver.com>
2024-02-27 17:09:26 +0900

    api#3

--- src/main/java/com/example/gamePT/domain/duoArticle/controller/DuoArticleController.java
+++ src/main/java/com/example/gamePT/domain/duoArticle/controller/DuoArticleController.java
@@ -1,21 +1,13 @@
 package com.example.gamePT.domain.duoArticle.controller;
 
 import com.example.gamePT.domain.duoArticle.enity.DuoArticle;
-import com.example.gamePT.domain.duoArticle.enity.DuoArticleForm;
 import com.example.gamePT.domain.duoArticle.service.DuoArticleService;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -26,50 +18,23 @@ public class DuoArticleController {
 
     @GetMapping("/list")
     public String list(Model model) throws IOException {
-        String apiUrl = "https://ddragon.leagueoflegends.com/cdn/14.4.1/data/en_US/champion/Aatrox.json";
-        RestTemplate restTemplate = new RestTemplate();
-        String jsonData = restTemplate.getForObject(apiUrl, String.class);
-
-        // JSON 데이터 파싱
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode championsNode = objectMapper.readTree(jsonData).get("data");
-
-        //이름만 저장
-        List<String> championNames = new ArrayList<>();
-        Iterator<String> championIterator = championsNode.fieldNames();
-        while (championIterator.hasNext()) {
-            String championName = championIterator.next();
-            championNames.add(championName);
-        }
-
-        // 가져온 JSON 데이터를 모델에 저장
-        model.addAttribute("championData", championNames);
-
-
-
         List<DuoArticle> duoArticleList = this.duoArticleService.getAllDuoArticles();
         model.addAttribute("duoArticleList", duoArticleList);
         return "duo/list";
     }
 
-    @GetMapping("/create")
-    public String create(DuoArticleForm duoArticleForm) {
-        return "duo/form";
-    }
-
     @PostMapping("/create")
-    public String create(@Valid DuoArticleForm duoArticleForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "duo/list";
-        }
-        this.duoArticleService.createDuoArticle(duoArticleForm.getMyLine(), duoArticleForm.getFindLine(),
-                duoArticleForm.getMicrophoneCheck(), duoArticleForm.getContent());
+    public String create(@RequestParam("myLine") String myLine, @RequestParam("findLine") String findLine,
+                         @RequestParam(name = "microphoneCheck", defaultValue = "false") boolean microphoneCheck,
+                         @RequestParam("content") String content) {
+
+        this.duoArticleService.createDuoArticle(myLine, findLine, microphoneCheck, content);
 
         return "redirect:/duo/list";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id")Long id) {
+    public String delete(@PathVariable("id") Long id) {
         DuoArticle duoArticle = this.duoArticleService.getDuoArticleById(id);
         this.duoArticleService.deleteDuoArticle(duoArticle);
         return "redirect:/duo/list";
