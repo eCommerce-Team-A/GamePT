9c404bab9856c98fb5b553f8772524b9ea701422
wintersky2 <emsw747@gmail.com>
2024-03-01 12:45:41 +0900

    CourseController.createCourse 리팩토링 완료

--- src/main/java/com/example/gamePT/domain/course/controller/CourseController.java
+++ src/main/java/com/example/gamePT/domain/course/controller/CourseController.java
@@ -24,40 +24,36 @@ import java.util.Optional;
 @RequestMapping("/course")
 public class CourseController {
     private final CourseService courseService;
-    private final UserRepository userRepository; // User 불러오기용 임시 의존성 주입. (createCourse 메서드에서 추후 작업 필요)
+    private final UserService userService;
     private final ReviewService reviewService;
+
+    //강의 등록
     @GetMapping("/create")
     public String createCourse() {
         return "course/course_create_form";
     }
 
+    // 강의 등록
     @PostMapping("/create")
     public String createCourse(@Valid CourseCreateForm courseCreateForm, BindingResult bindingResult, Principal principal) {
-        //임시 siteUser 부분 시작
-        SiteUser siteUser = null;
-        if (principal != null) {
-            Optional<SiteUser> _siteUser = this.userRepository.findByUsername(principal.getName());
-            if (_siteUser.isPresent()) {
-                siteUser = _siteUser.get();
-            }
-        }
-        // 임시 siteUser 부분 끝
-
+        SiteUser siteUser = this.userService.findByUsername(principal.getName());
         if (bindingResult.hasErrors()) {
             return "course/course_create_form";
         }
         Course course = this.courseService.createCourse(siteUser, courseCreateForm.getGameCategoryname(), courseCreateForm.getName(),
                 courseCreateForm.getIntroduce(), courseCreateForm.getCurriculum(), courseCreateForm.getPrice());
+
         return String.format("redirect:/course/detail/%s", course.getId());
     }
 
+    //강의 상세 페이지
     @GetMapping("/detail/{id}")
     public String showCourseDetail(@PathVariable(value = "id") Long id, Model model) {
         Course course = this.courseService.findCourseById(id);
         List<Course> courseList = this.courseService.findAllCourse();
         List<Review> reviewList = this.reviewService.findByCourseId(id);
 
-        model.addAttribute("reviewList",reviewList);
+        model.addAttribute("reviewList", reviewList);
         model.addAttribute("courseList", courseList);
         model.addAttribute("course", course);
 
