5511ce02d3e2f26222ec2eb575fafb4f56fe72b3
wintersky <148525670+wintersky2@users.noreply.github.com>
2024-03-05 17:04:35 +0900

    Feat/17 (#33)
    * 콜백함수 해야함
    
    * CourseController.createCourse 리팩토링 완료
    
    * reviewcreate ajax 에서 success 함수 수정필요
    
    * ajax post요청 끝나고 script 먹통문제해결중
    
    * ajax post요청 끝나고 script 먹통문제해결중
    
    * ajax 이후 문제 해결 및 리뷰 날짜순 정렬, 리뷰 작성후 양식 초기화 완료
    
    * 리뷰 부분 완료
    
    * 리뷰 부분 완료
    
    * 리뷰 부분 완료-3
    
    * 리뷰 부분 완료-4
    
    * 리뷰 부분 완료-5
    
    * 리뷰 부분 완료-6

--- src/main/java/com/example/gamePT/domain/course/repository/CourseRepository.java
+++ src/main/java/com/example/gamePT/domain/course/repository/CourseRepository.java
@@ -3,5 +3,8 @@ package com.example.gamePT.domain.course.repository;
 import com.example.gamePT.domain.course.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findCourseByAuthorId(Long id);
 }
