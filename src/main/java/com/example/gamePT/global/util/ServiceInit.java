5172a8d3f9fb58529ebf2144740aa70efda00c45
hdYang <gmlehd0201@gmail.com>
2024-03-08 17:40:34 +0900

    메인 수ì가데이터 완성

--- src/main/java/com/example/gamePT/global/util/ServiceInit.java
+++ src/main/java/com/example/gamePT/global/util/ServiceInit.java
@@ -2,9 +2,15 @@ package com.example.gamePT.global.util;
 
 import com.example.gamePT.domain.course.entity.Course;
 import com.example.gamePT.domain.course.repository.CourseRepository;
+import com.example.gamePT.domain.duoArticle.enity.DuoArticle;
+import com.example.gamePT.domain.duoArticle.repository.DuoArticleRepository;
 import com.example.gamePT.domain.image.service.ImageService;
+import com.example.gamePT.domain.qna.entity.QnA;
+import com.example.gamePT.domain.qna.repository.QnARepository;
 import com.example.gamePT.domain.user.entity.SiteUser;
 import com.example.gamePT.domain.user.repository.UserRepository;
+import com.example.gamePT.global.riot.entity.LeagueDTO;
+import com.example.gamePT.global.riot.entity.SummonerDTO;
 import com.example.gamePT.global.riot.service.RiotApiService;
 import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +32,8 @@ public class ServiceInit implements InitializingBean {
     private final UserRepository userRepository;
     private final ImageService imageService;
     private final RiotApiService riotApiService;
+    private final QnARepository qnARepository;
+    private final DuoArticleRepository duoArticleRepository;
 
     @Override
     public void afterPropertiesSet() throws Exception {
@@ -62,7 +70,7 @@ public class ServiceInit implements InitializingBean {
                 "항개만줘쓰라 KR1",
                 "롤체홍선생 KR1",
                 "손희원 KR1",
-                "김계식 KR1",
+                "가드라 KR1",
                 "Meloey KR1",
                 "경안이 KR1",
                 "류지한 KR1",
@@ -70,6 +78,9 @@ public class ServiceInit implements InitializingBean {
                 "고양이 춘식이",
                 "후랭이 KR1",
                 "쭈니대박 KR1"};
+        String[] lineList = {"fill","top","jungle","middle","bottom","utility"};
+
+        int k = 0;
 
         for(int i = 0 ; i<10; i++){
 
@@ -100,6 +111,31 @@ public class ServiceInit implements InitializingBean {
                     build();
 
             courseRepository.save(cu);
+
+            SummonerDTO summonerDTO = this.riotApiService.getSummoner(su2.getPuuid());
+            LeagueDTO leagueDTO = this.riotApiService.getLeague(summonerDTO.getId());
+
+            if(k+1 > 5){
+                k = 0;
+            }
+
+            DuoArticle duoArticle = DuoArticle.builder()
+                    .myLine(lineList[k])
+                    .findLine(lineList[k+1])
+                    .microphoneCheck(i%2 == 0)
+                    .username(su2.getUsername())
+                    .puuid(su2.getPuuid())
+                    .content(su2.getNickname()+"이랑 게임해요")
+                    .gameName(su2.getGameName())
+                    .tag(su2.getTag())
+                    .profileIconId(summonerDTO.getProfileIconId())
+                    .tier(leagueDTO.getTier())
+                    .rank(leagueDTO.getTier().charAt(0) + "" + this.getNum(leagueDTO.getRank()))
+                    .wins(leagueDTO.getWins())
+                    .losses(leagueDTO.getLosses())
+                    .build();
+            duoArticleRepository.save(duoArticle);
+            k++;
         }
 
         for(int i = 10 ; i<20; i++){
@@ -120,22 +156,64 @@ public class ServiceInit implements InitializingBean {
 
             su2 = userRepository.save(su2);//자신이 호출하고싶은 메소드
             imageService.saveUserProfile(su2, getMultipartFile());
+            QnA qna = new QnA();
+            if(i%2 == 0){
+                qna = QnA.builder().
+                        author(su2).
+                        title(su2.getNickname() + "의 문의문의").
+                        content(su2.getNickname() + "의 나문희 내용 이에요~").
+                        isBlind(false).
+                        isAnswered(false).
+                        build();
+            }else{
+                qna = QnA.builder().
+                        author(su2).
+                        title(su2.getNickname() + "의 문의문의").
+                        content(su2.getNickname() + "의 나문희 내용 이에요~").
+                        isBlind(true).
+                        isAnswered(false).
+                        build();
+            }
+            qnARepository.save(qna);
 
-            Course cu = Course.builder().
-                    author(su2).
-                    gameCategoryname("LOL").
-                    name(su2.getNickname() + "의 기가막힌 강의").
-                    introduce(su2.getNickname() + "강의의 예시 데이터에요. 같이 잘 해보아요!").
-                    curriculum(su2.getNickname() + "의 강의는 고객이 만족할때까지 쉬지 않고 계속해요").
-                    price((int)((Math.random() * 899) + 100)*1000).
-                    isActive(true).
-                    build();
+            SummonerDTO summonerDTO = this.riotApiService.getSummoner(su2.getPuuid());
+            LeagueDTO leagueDTO = this.riotApiService.getLeague(summonerDTO.getId());
 
-            courseRepository.save(cu);
+            if(k+1 > 5){
+                k = 0;
+            }
+
+            DuoArticle duoArticle = DuoArticle.builder()
+                    .myLine(lineList[k])
+                    .findLine(lineList[k+1])
+                    .microphoneCheck(i%2 == 0)
+                    .username(su2.getUsername())
+                    .puuid(su2.getPuuid())
+                    .content(su2.getNickname()+"이랑 게임해요")
+                    .gameName(su2.getGameName())
+                    .tag(su2.getTag())
+                    .profileIconId(summonerDTO.getProfileIconId())
+                    .tier(leagueDTO.getTier())
+                    .rank(leagueDTO.getTier().charAt(0) + "" + this.getNum(leagueDTO.getRank()))
+                    .wins(leagueDTO.getWins())
+                    .losses(leagueDTO.getLosses())
+                    .build();
+
+            duoArticleRepository.save(duoArticle);
+
+            k++;
         }
 
     }
-
+    public Integer getNum(String rank) {
+        return switch (rank) {
+            case "I" -> 1;
+            case "II" -> 2;
+            case "III" -> 3;
+            case "IV" -> 4;
+            default -> null;
+        };
+    }
 
 
     public MultipartFile getMultipartFile(){
