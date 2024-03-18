3ef798ee8871bff44db4de81dfad3639319a1ef7
5binn <dhxnejqls13@naver.com>
2024-03-18 17:22:59 +0900

    강의 리스트 내용, 카테고리 -> 리뷰

--- src/main/java/com/example/gamePT/domain/course/entity/CourseCreateForm.java
+++ src/main/java/com/example/gamePT/domain/course/entity/CourseCreateForm.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CourseCreateForm {//임시로 (전문가 이름) 삽입 추후 siteUser .expert 객체 추가 필요
-    private String gameCategoryname; // 임시로 (카테고리 이름) 삽입 추후 gameCategory 객체추가 필요
     private String name;
     private String introduce;
     private String curriculum;
