cba7c002cd66bda4010467d8d2dfe429c7b94c58
5binn <dhxnejqls13@naver.com>
2024-03-18 16:44:01 +0900

    강의 리스트 수정

--- src/main/java/com/example/gamePT/domain/course/entity/Course.java
+++ src/main/java/com/example/gamePT/domain/course/entity/Course.java
@@ -1,13 +1,18 @@
 package com.example.gamePT.domain.course.entity;
 
+import com.example.gamePT.domain.review.entity.Review;
 import com.example.gamePT.domain.user.entity.SiteUser;
 import com.example.gamePT.global.jpa.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,6 +22,8 @@ import lombok.experimental.SuperBuilder;
 public class Course extends BaseEntity {
     @ManyToOne
     private SiteUser author;
+    @OneToMany(mappedBy = "course",cascade = CascadeType.REMOVE)
+    private List<Review> reviewList;
     private String name;
     private String introduce;
     private String curriculum;
